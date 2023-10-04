b63c729389cdd9a3f8314010b1ec50c7b3097a7f
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-03 22:32:11 -0300

    feat: wip

--- futemax-admin/database/migrations/2014_10_12_000000_create_users_table.php
+++ futemax-admin/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,19 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('cod_usuario');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
