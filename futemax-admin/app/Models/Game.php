c842b7547c009ac9d62ff0632d08935a0fcb9b03
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-09 23:01:48 -0300

    feat: wip

---
+++ futemax-admin/app/Models/Game.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'game_day',
+        'game_name',
+    ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
