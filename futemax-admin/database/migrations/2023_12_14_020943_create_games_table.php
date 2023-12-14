d5a71be07b209c8525f3cd7f281d0123a62ab455
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-12-13 23:10:47 -0300

    wip

--- futemax-admin/database/migrations/2023_12_14_020943_create_games_table.php
+++ futemax-admin/database/migrations/2023_12_14_020943_create_games_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->integer('sport_id');
             $table->timestamps();
         });
     }
