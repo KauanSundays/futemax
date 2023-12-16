e3d4fbec55d0c375939ae5af449324f54cc7d7df
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-12-15 23:31:21 -0300

    redirect

--- futemax-admin/routes/web.php
+++ futemax-admin/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redirect;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Redirect::to('/admin/login');
 });
