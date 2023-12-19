aab5783a8b2f09ef742bca8320dacff3e791c614
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-12-18 21:57:13 -0300

    wip

---
+++ futemax-admin/app/Filament/Resources/SportResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SportResource\Pages;
+use App\Filament\Resources\SportResource\RelationManagers;
+use App\Models\Sport;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SportResource extends Resource
+{
+    protected static ?string $model = Sport::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSports::route('/'),
+            'create' => Pages\CreateSport::route('/create'),
+            'edit' => Pages\EditSport::route('/{record}/edit'),
+        ];
+    }
+}
