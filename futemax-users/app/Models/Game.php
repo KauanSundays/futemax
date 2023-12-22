89d950e94e986964ee647617a82a6708a4781e69
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-12-22 10:36:46 -0300

    wip

---
+++ futemax-users/app/Models/Game.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name_game','sport_id', 'is_live', 'game_image'];
+
+    public function sport()
+    {
+        return $this->belongsTo(Sport::class);
+    }
+}
