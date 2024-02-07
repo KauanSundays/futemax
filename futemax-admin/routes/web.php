8e7b1d818cb4997ec691d1d984b7d71f8a37deca
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-02-07 06:42:18 -0300

    resolve route

--- futemax-admin/routes/web.php
+++ futemax-admin/routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Redirect;
-
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return Redirect::to('/admin/login');
+    return view('welcome');
 });
